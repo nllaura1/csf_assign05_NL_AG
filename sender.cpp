9111958cf4b0917f58af7ecd310a7623e83079e9
Nicholas <nllaura1@jhu.edu>
2025-04-17 15:47:50 -0400

    sender changes

--- sender.cpp
+++ sender.cpp
@@ -13,20 +13,94 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  std::string server_hostname;
-  int server_port;
-  std::string username;
+  std::string server_hostname = argv[1];
+  int server_port = std::stoi(argv[2]);
+  std::string username = argv[3];
 
-  server_hostname = argv[1];
-  server_port = std::stoi(argv[2]);
-  username = argv[3];
+  Connection conn;
 
-  // TODO: connect to server
- 
-  // TODO: send slogin message
+  if (!conn.connect(server_hostname, server_port)) {
+    std::cerr << "Error: could not connect to server.\n";
+    return 1;
+  }
 
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
+  // Step 1: send slogin
+  Message login_msg(TAG_SLOGIN, username);
+  if (!conn.send(login_msg)) {
+    std::cerr << "Error: failed to send slogin message.\n";
+    return 1;
+  }
+
+  Message reply;
+  if (!conn.receive(reply)) {
+    std::cerr << "Error: failed to receive reply from server.\n";
+    return 1;
+  }
+
+  if (reply.tag == TAG_ERR) {
+    std::cerr << reply.data << std::endl;
+    return 1;
+  }
+
+  // Step 2: command/message loop
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    if (line.empty()) continue;
+
+    Message out_msg;
+
+    if (line[0] == '/') {
+      std::istringstream iss(line);
+      std::string cmd;
+      iss >> cmd;
+
+      if (cmd == "/join") {
+        std::string room;
+        if (iss >> room) {
+          out_msg.tag = TAG_JOIN;
+          out_msg.data = room;
+        } else {
+          std::cerr << "Usage: /join [room_name]" << std::endl;
+          continue;
+        }
+      } else if (cmd == "/leave") {
+        out_msg.tag = TAG_LEAVE;
+        out_msg.data = "";
+      } else if (cmd == "/quit") {
+        out_msg.tag = TAG_QUIT;
+        out_msg.data = "bye"; // payload can be arbitrary but must be non-empty
+        conn.send(out_msg);
+        if (conn.receive(reply) && reply.tag == TAG_OK) {
+          return 0;
+        } else {
+          std::cerr << (reply.tag == TAG_ERR ? reply.data : "Unknown error") << std::endl;
+          return 1;
+        }
+      } else {
+        std::cerr << "Unknown command: " << cmd << std::endl;
+        continue;
+      }
+    } else {
+      // Send as message
+      out_msg.tag = TAG_SENDALL;
+      out_msg.data = line;
+    }
+
+    // Send the message and wait for response
+    if (!conn.send(out_msg)) {
+      std::cerr << "Error: failed to send message.\n";
+      return 1;
+    }
+
+    if (!conn.receive(reply)) {
+      std::cerr << "Error: failed to receive reply from server.\n";
+      return 1;
+    }
+
+    if (reply.tag == TAG_ERR) {
+      std::cerr << reply.data << std::endl;
+    }
+  }
 
   return 0;
 }
