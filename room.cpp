797141a2d06e5df3c8768627d805efc0a81aa960
Antony Goldenberg <goldenberg.antony@gmail.com>
2025-04-27 16:05:40 -0400

    Added message queue and room

--- room.cpp
+++ room.cpp
@@ -6,21 +6,40 @@
 
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
-  // TODO: initialize the mutex
+  // Initialize the mutex
+  pthread_mutex_init(&lock, nullptr);
 }
 
 Room::~Room() {
-  // TODO: destroy the mutex
+  // Destroy the mutex
+  pthread_mutex_destroy(&lock);
+  
+  // Note: We don't delete User objects here as they're owned by the Server
 }
 
 void Room::add_member(User *user) {
-  // TODO: add User to the room
+  Guard guard(lock); // RAII lock acquisition
+  
+  // Add user to the members set
+  members.insert(user);
 }
 
 void Room::remove_member(User *user) {
-  // TODO: remove User from the room
+  Guard guard(lock); // RAII lock acquisition
+  
+  // Remove user from the members set
+  members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
-  // TODO: send a message to every (receiver) User in the room
+  Guard guard(lock);
+  
+  // Create properly formatted message using existing constructor
+  std::string payload = room_name + ":" + sender_username + ":" + message_text;
+  Message *msg = new Message(TAG_DELIVERY, payload);
+  
+  // Send to all members
+  for (User *user : members) {
+    user->mqueue.enqueue(msg);
+  }
 }
